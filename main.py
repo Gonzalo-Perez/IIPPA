a4f9db4d20304f5c8e17c298695f35f5a34bbcc5
Gonzalo-Perez <gaperez2@uc.cl>
2018-06-01 20:36:46 -0400

    Updating test images folder

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from iter_methods import *
 
 N = 15
 
-# Test change
+
 if __name__ == "__main__":
     img_objective = cv2.imread("circulos_coloridos.png")
     img_objective = (img_objective / 255)

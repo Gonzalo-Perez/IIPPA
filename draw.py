bug fix: gradient not considering colors, and other details

draw.py
@@ -76,7 +76,7 @@ def draw_image_2(vars, H, W, background_color=(1., 1., 1.), background_alpha=.2)
     RGB = list(vars[:, 6:9])  # colors
     RGB.insert(0, background_color)
     RGB = np.asarray(RGB)
-    RGB = RGB * (RGB >= 0)  # Colors must be positive
+    RGB = RGB * (RGB < 1) * (RGB >= 0) + (RGB >= 1) # Colors must be between 0 and 1.
     out = shape_matrix @ RGB  # the matrix multiplication magic
     for i in range(3):  # Normalization
         out[:, :, i] = (out[:, :, i] / alpha_sum)
@@ -120,7 +120,7 @@ def draw_multi_image_2(vars, H, W, index, perturbations, background_color=(1., 1
     RGB = list(np.asarray(v)[:, 6:9])  # colors N - 1
     RGB.insert(0, background_color)  # insert manually the background
     RGB = np.asarray(RGB)
-    RGB = RGB * (RGB >= 0)  # Colors must be positive
+    RGB = RGB * (RGB < 1) * (RGB >= 0) + (RGB >= 1)  # Colors must be between 0 and 1.
     out = shape_matrix @ RGB  # the matrix multiplication magic
 
     outs = np.zeros((K, H, W, 3))
@@ -149,11 +149,12 @@ def draw_multi_image_2(vars, H, W, index, perturbations, background_color=(1., 1
         a_sum = alpha_sum + lay
         lay.shape = H, W, 1
         col = np.asarray(u[6:9])
-        col = col * (col >= 0)
+        col = col * (col >= 0) * (col < 1) + (col >= 1) # Colors must be between 0 and 1.
         col.shape = 1, 3
-        outs[k] = out + lay @ col
+        last = lay @ col
+        outs[k] = out + last
         for i in range(3):  # Normalization
-            outs[k][:, :, i] = (out[:, :, i] / a_sum)
+            outs[k][:, :, i] = (outs[k][:, :, i] / a_sum)
     if return_submatrix_coords:
         return outs, (s_x, e_x, s_y, e_y)
     else:

iter_methods.py
@@ -6,7 +6,7 @@ from gradients import *
 from draw import *
 
 
-def get_random_start_2(N, H, W):
+def get_random_start_2(N):
     """ USED DOUBLES FOR POSITIONS
     Returns a random set of coordinates for the
     :param seed: integer
@@ -19,8 +19,33 @@ def get_random_start_2(N, H, W):
             vars[i] = np.array([np.random.uniform(), np.random.uniform(),
                                 np.random.uniform(), np.random.uniform(),
                                 np.random.uniform(), np.random.uniform(),
-                                np.random.uniform(),
-                                np.random.uniform(),
+                                np.random.uniform(), # blue
+                                np.random.uniform(), # green
+                                np.random.uniform(), # red
+                                np.random.uniform()])
+        else:
+            vars[i] = np.array([np.random.uniform(), np.random.uniform(),
+                                np.random.uniform(), np.random.uniform(),
+                                np.random.uniform(), np.random.uniform(),
+                                0, 0, 0, 10])
+    return vars
+
+
+def get_red_random_start_2(N):
+    """ USED DOUBLES FOR POSITIONS
+    Returns a random set of coordinates for the
+    :param seed: integer
+    :return: vars
+    """
+    black = False
+    vars = np.zeros((N, 10))
+    for i in range(N):
+        if not black:
+            vars[i] = np.array([np.random.uniform(), np.random.uniform(),
+                                np.random.uniform(), np.random.uniform(),
+                                np.random.uniform(), np.random.uniform(),
+                                0,
+                                0,
                                 np.random.uniform(),
                                 np.random.uniform()])
         else:
@@ -48,7 +73,8 @@ def simple_gradient_method(target_image, N, norm, step, max_iter, tol, _delta=.2
     """
     H = target_image.shape[0]
     W = target_image.shape[1]
-    x_i = get_random_start_2(N, H, W)
+    x_i = get_random_start_2(N)
+    # x_i = get_red_random_start_2(N)
     it = 0
     while it < max_iter:
         print('computing gradient...')
@@ -56,10 +82,16 @@ def simple_gradient_method(target_image, N, norm, step, max_iter, tol, _delta=.2
         grad = numerical_grad(x_i, norm, target_image, delta=_delta, _scheme=diff_scheme_to_use,
                               parallel=use_threads)
         print("Iteration: {0}, Elapsed time: {1}".format(it, time.time() - tt))
-        print("x: {0}".format(x_i))
-        x_next = x_i - step(it) * grad
+        # print("x: {0}".format(x_i))
+        x_next = update_x(x_i, grad, step(it), color_boundaries=True, vertex_boundaries=False)
         difference = norm(draw_image_2(x_i, H, W), target_image)
-        print("Gradient: {0}".format(grad))
+        # print("Gradient: {0}".format(grad))
+        print('Gradient:')
+        for i in range(10):
+            if i <= 5:
+                print('vertex:', grad[:, i])
+            else:
+                print('color:', grad[:, i])
         print("Difference from target: {0}".format(difference))
         if difference < tol:
             x_i = x_next
@@ -74,6 +106,27 @@ def simple_gradient_method(target_image, N, norm, step, max_iter, tol, _delta=.2
 
         it += 1
     return x_i
+
+
+def update_x(x_i, grad, step, color_boundaries=True, vertex_boundaries=False):
+    """
+    returns the updated x_i, checks and corrects boundaries of color and vertex positions.
+    :param x_i: 
+    :param grad: 
+    :param step: 
+    :param color_boundaries: 
+    :param vertex_boundaries: 
+    :return: 
+    """
+    x = x_i - step * grad
+    #check alpha positive
+    x[:, 9:10] = x[:, 9:10] * (x[:, 9:10] >= 0)
+    if color_boundaries:
+        x[:,6:9] = x[:,6:9] * (x[:,6:9] < 1) * (x[:,6:9] >= 0) + (x[:,6:9] >= 1)
+    if vertex_boundaries:
+        x[:, 0:6] = x[:, 0:6] * (x[:, 0:6] < 1) * (x[:, 0:6] >= 0) + (x[:, 0:6] >= 1)
+    return x
+
 
 
 # NEEDS REFACTORING

main.py
@@ -6,13 +6,14 @@ from measures import *
 N = 15
 
 if __name__ == "__main__":
-    img_objective = cv2.imread("Test_cases/blue_circle.png")
+    img_objective = cv2.imread("Test_cases/circulos_coloridos.png")
     img_objective = (img_objective / 255)
-
+    np.random.seed(1235)
     cv2.namedWindow("Objetivo Simple")
     cv2.moveWindow("Objetivo Simple", 500, 250)
     cv2.imshow("Objetivo Simple", img_objective)
     cv2.waitKey(0)
+
 
     x = simple_gradient_method(img_objective, N, MSE, lambda n: .3 / np.log(np.e + n), 1000, 1e-9, _delta=.05,
                                diff_scheme_to_use=2, use_threads=False, show_progress=True)